c89fd4b2f4f5af44842b513a2ec52155b656ee01
Preslav Karatanev <pepopapa@gmail.com>
2021-01-31 14:25:22 +0200

    Add some startup models and components

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@pitstop.com',
+            'password' => Hash::make('demo'),
+        ]);
+    }
+}
