a2214eebd7240929428b2c0815a5d9d3449dd32a
pkaratanev <peporapa@gmail.com>
2022-02-01 20:20:56 +0200

    Fix shop location update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,17 +4,10 @@ require('./bootstrap');
 import Vue from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
 import PortalVue from 'portal-vue';
-import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
-
-Vue.use(VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyBOQ_Rz2YcAxZzrzO5S1Uiv_XCjuif2sLM'
-    },
-});
 
 const app = document.getElementById('app');
 
