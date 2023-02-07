3ba2f3d2457401c86cd73483cdc71093742b9bfb
Preslav <peporapa@gmail.com>
2023-02-07 18:38:18 +0200

    Update codebase

--- resources/js/Components/Appointments/Form.vue
+++ resources/js/Components/Appointments/Form.vue
@@ -1,120 +1,154 @@
 <template>
-	<div class="w-full px-8 pt-6 pb-8 mb-4">
-		<form @submit="createAppointment()">
-			<div class="mb-4">
-				<label class="block font-medium text-sm text-gray-700" for="username">
-					Subject
-				</label>
+    <div class="w-full px-8 pt-6 pb-8 mb-4">
+        <form @submit="createAppointment()">
+            <div class="mb-4">
+                <label
+                    class="block font-medium text-sm text-gray-700"
+                    for="username"
+                >
+                    Subject
+                </label>
 
-				<input
-					class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
-					id="username"
-					type="text"
-					placeholder="Brief explanation of your appointment"
+                <input
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                    id="username"
+                    type="text"
+                    placeholder="Brief explanation of your appointment"
                     v-model="form.subject"
                     required
-				>
-			</div>
+                />
+            </div>
 
-			<div class="mb-4">
-				<label class="block font-medium text-sm text-gray-700" for="username">
-					Date & Time
-				</label>
+            <div class="mb-4">
+                <label
+                    class="block font-medium text-sm text-gray-700"
+                    for="username"
+                >
+                    Date & Time
+                </label>
 
-				<div class="relative">
-					<div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-						<svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-					</div>
+                <div class="relative">
+                    <div
+                        class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                    >
+                        <svg
+                            class="w-5 h-5 text-gray-500"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </div>
 
-					<input
+                    <input
                         ref="datepicker"
                         type="text"
                         class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full pl-10"
                         placeholder="Select date"
                         required
+                    />
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <label
+                    class="block font-medium text-sm text-gray-700"
+                    for="username"
+                >
+                    Car
+                </label>
+
+                <select
+                    v-model="form.car"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                >
+                    <option
+                        v-for="car in JSON.parse(user.cars)"
+                        :key="car.model"
+                        :value="car"
                     >
-				</div>
-			</div>
+                        {{ car.make }} - {{ car.model }} - {{ car.year }}
+                    </option>
+                </select>
+            </div>
 
-			<div class="mb-4">
-				<label class="block font-medium text-sm text-gray-700" for="username">
-					Car
-				</label>
+            <div class="mb-4">
+                <label
+                    class="block font-medium text-sm text-gray-700"
+                    for="username"
+                >
+                    Details
+                </label>
 
-				<select v-model="form.car" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full">
-					<option v-for="car in JSON.parse(user.cars)" :key="car.model" :value="car">{{car.make}} - {{car.model}} - {{car.year}}</option>
-				</select>
-			</div>
-
-			<div class="mb-4">
-				<label class="block font-medium text-sm text-gray-700" for="username">
-					Details
-				</label>
-
-				<textarea
-					class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
-					id="username"
-					type="text"
+                <textarea
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                    id="username"
+                    type="text"
                     rows="4"
-					placeholder="More detailed explanation of your request"
+                    placeholder="More detailed explanation of your request"
                     v-model="form.details"
                     required
                 >
-				</textarea>
-			</div>
+                </textarea>
+            </div>
 
-			<div class="flex items-center justify-between">
-				<button
-					class="mt-4 py-2 px-4 border border-transparent shadow-sm text-sm rounded-md font-bold text-white bg-indigo-400 hover:bg-indigo-700"
-					type="button"
+            <div class="flex items-center justify-between">
+                <button
+                    class="mt-4 py-2 px-4 border border-transparent shadow-sm text-sm rounded-md font-bold text-white bg-indigo-400 hover:bg-indigo-700"
+                    type="button"
                     @click="createAppointment()"
-				>
-					Create Appointment
-				</button>
-			</div>
-		</form>
-	</div>
+                >
+                    Create Appointment
+                </button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-	import Textarea from '../../Jetstream/Textarea.vue'
-	import Datepicker from '@themesberg/tailwind-datepicker/Datepicker';
+import Textarea from "../../Jetstream/Textarea.vue";
+import Datepicker from "@themesberg/tailwind-datepicker/Datepicker";
 
-	export default {
-        name: 'Form',
+export default {
+    name: "Form",
 
-		components: {
-			Textarea,
-			Datepicker
-		},
+    components: {
+        Textarea,
+        Datepicker,
+    },
 
-        props: {
-            newAppointmentShop: Object,
-            user: Object
+    props: {
+        newAppointmentShop: Object,
+        user: Object,
+    },
+
+    data() {
+        return {
+            form: this.$inertia.form({
+                subject: null,
+                datetime: null,
+                car: null,
+                details: null,
+                shopId: this.newAppointmentShop.id,
+            }),
+        };
+    },
+
+    mounted() {
+        new Datepicker(this.$refs.datepicker);
+    },
+
+    methods: {
+        createAppointment() {
+            this.form.datetime = this.$refs.datepicker.value;
+
+            this.$emit("createAppointment", this.form);
         },
-
-        data() {
-            return {
-                form: this.$inertia.form({
-                    subject: null,
-                    datetime: null,
-                    car: null,
-                    details: null,
-                    shopId: this.newAppointmentShop.id
-                })
-            }
-        },
-
-		mounted() {
-            new Datepicker(this.$refs.datepicker)
-        },
-
-        methods: {
-            createAppointment() {
-                this.form.datetime = this.$refs.datepicker.value
-
-                this.$emit('createAppointment', this.form)
-            }
-        }
-	}
+    },
+};
 </script>
--- resources/js/Pages/Profile/UpdatePersonalCarsForm.vue
+++ resources/js/Pages/Profile/UpdatePersonalCarsForm.vue
@@ -1,8 +1,6 @@
 <template>
     <jet-form-section @submitted="updateProfileCars">
-        <template #title>
-            Personal Vehicles
-        </template>
+        <template #title> Personal Vehicles </template>
 
         <template #description>
             Add or change your personal vehicles (make, model, year)
@@ -10,68 +8,112 @@
 
         <template #extra>
             <div class="grid grid-cols-3 -ml-2 -mr-2" v-if="newCars">
-                <div class="bg-gray-50 p-4 ml-2 mr-2 mb-4 border-gray-300 rounded-md shadow-sm relative" v-for="(car, index) in newCars" :key="index">
-                    <div><span class="font-bold">Make:</span> {{car.make}}</div>
+                <div
+                    class="bg-gray-50 p-4 ml-2 mr-2 mb-4 border-gray-300 rounded-md shadow-sm relative"
+                    v-for="(car, carIndex) in newCars"
+                    :key="carIndex"
+                >
+                    <div>
+                        <span class="font-bold">Make:</span> {{ car.make }}
+                    </div>
 
-                    <div><span class="font-bold">Model:</span> {{car.model}}</div>
+                    <div>
+                        <span class="font-bold">Model:</span> {{ car.model }}
+                    </div>
 
-                    <div><span class="font-bold">Year:</span> {{car.year}}</div>
+                    <div>
+                        <span class="font-bold">Year:</span> {{ car.year }}
+                    </div>
 
-                    <a href="#" class="block absolute top-2 right-4" @click.prevent="removeCar(index)">
+                    <a
+                        href="#"
+                        class="block absolute top-2 right-4"
+                        @click.prevent="removeCar(carIndex)"
+                    >
                         <i class="fa fa-times" aria-hidden="true"></i>
                     </a>
                 </div>
             </div>
 
-            <button type="button" @click="addNewVehicle()" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+            <button
+                type="button"
+                @click="addNewVehicle()"
+                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150"
+            >
                 Add New Vehicle
             </button>
 
-            <div class="grid grid-cols-6 gap-6 mt-8" v-if="formState === 'stepOne' || formState === 'stepTwo' || formState === 'stepThree'">
+            <div
+                class="grid grid-cols-6 gap-6 mt-8"
+                v-if="
+                    formState === 'stepOne' ||
+                    formState === 'stepTwo' ||
+                    formState === 'stepThree'
+                "
+            >
                 <!-- Make -->
-                <div class="col-span-6 sm:col-span-4" v-if="formState === 'stepOne' && carMakes">
+                <div
+                    class="col-span-6 sm:col-span-4"
+                    v-if="formState === 'stepOne'"
+                >
                     <jet-label for="make" value="Search Vehicle Make" />
 
-                    <ul class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                        <li v-for="(carMake, index) in carMakes"
-                            :key="index"
+                    <ul
+                        class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                    >
+                        <li
+                            v-for="(carMake, carMakeIndex) in carMakes"
+                            :key="'carMakeIndex-' + carMakeIndex"
+                            :id="'carMakeIndex-' + carMakeIndex"
                             class="pl-2 pt-1 pb-1 cursor-pointer hover:bg-indigo-300"
-                            :class="{'bg-gray-50': index % 2 === 0}"
-                            @click="selectMake(carMake)"
+                            :class="{ 'bg-gray-50': carMakeIndex % 2 === 0 }"
+                            @click.prevent="selectMake(carMake)"
                         >
-                            {{carMake}}
+                            {{ carMake }}
                         </li>
                     </ul>
                 </div>
 
                 <!-- Model -->
-                <div class="col-span-6 sm:col-span-4" v-if="formState === 'stepTwo' && carModels">
+                <div
+                    class="col-span-6 sm:col-span-4"
+                    v-if="formState === 'stepTwo'"
+                >
                     <jet-label for="model" value="Search Vehicle Model" />
 
-                    <ul class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                        <li v-for="(carModel, index) in carModels"
-                            :key="index"
+                    <ul
+                        class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                    >
+                        <li
+                            v-for="(carModel, carModelIndex) in carModels"
+                            :key="'carModelIndex-' + carModelIndex"
                             class="pl-2 pt-1 pb-1 cursor-pointer hover:bg-indigo-300"
-                            :class="{'bg-gray-50': index % 2 === 0}"
-                            @click="selectModel(carModel)"
+                            :class="{ 'bg-gray-50': carModelIndex % 2 === 0 }"
+                            @click.prevent="selectModel(carModel)"
                         >
-                            {{carModel.model}}
+                            {{ carModel.model }}
                         </li>
                     </ul>
                 </div>
 
                 <!-- Year -->
-                <div class="col-span-6 sm:col-span-4" v-if="formState === 'stepThree' && carYears">
+                <div
+                    class="col-span-6 sm:col-span-4"
+                    v-if="formState === 'stepThree' && carYears"
+                >
                     <jet-label for="year" value="Search Vehicle Year" />
 
-                    <ul class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                        <li v-for="(carYear, index) in carYears"
-                            :key="index"
+                    <ul
+                        class="border mt-2 block max-h-32 overflow-x-auto border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                    >
+                        <li
+                            v-for="(carYear, carYearIndex) in carYears"
+                            :key="'carYearIndex-' + carYearIndex"
                             class="pl-2 pt-1 pb-1 cursor-pointer hover:bg-indigo-300"
-                            :class="{'bg-gray-50': index % 2 === 0}"
+                            :class="{ 'bg-gray-50': carYearIndex % 2 === 0 }"
                             @click="selectYear(carYear)"
                         >
-                            {{carYear}}
+                            {{ carYear }}
                         </li>
                     </ul>
                 </div>
@@ -83,7 +125,8 @@
                 Saved.
             </jet-action-message>
 
-            <button @click.prevent="updateProfileCars()"
+            <button
+                @click.prevent="updateProfileCars()"
                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150"
                 :class="{ 'opacity-25': processing }"
                 :disabled="processing"
@@ -95,127 +138,133 @@
 </template>
 
 <script>
-    import JetButton from '@/Jetstream/Button'
-    import JetFormSection from '@/Jetstream/FormSection'
-    import JetInput from '@/Jetstream/Input'
-    import JetInputError from '@/Jetstream/InputError'
-    import JetLabel from '@/Jetstream/Label'
-    import JetActionMessage from '@/Jetstream/ActionMessage'
-    import JetSecondaryButton from '@/Jetstream/SecondaryButton'
+import JetButton from "@/Jetstream/Button";
+import JetFormSection from "@/Jetstream/FormSection";
+import JetInput from "@/Jetstream/Input";
+import JetInputError from "@/Jetstream/InputError";
+import JetLabel from "@/Jetstream/Label";
+import JetActionMessage from "@/Jetstream/ActionMessage";
+import JetSecondaryButton from "@/Jetstream/SecondaryButton";
 
-    export default {
-        components: {
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-            JetSecondaryButton,
+export default {
+    components: {
+        JetActionMessage,
+        JetButton,
+        JetFormSection,
+        JetInput,
+        JetInputError,
+        JetLabel,
+        JetSecondaryButton,
+    },
+
+    props: ["user"],
+
+    data() {
+        return {
+            formState: "none",
+            carMakes: null,
+            carModels: null,
+            carYears: null,
+            processing: false,
+            recentlySuccessful: false,
+            newCars: null,
+        };
+    },
+
+    mounted() {
+        this.newCars = JSON.parse(this.user.cars);
+    },
+
+    methods: {
+        addNewVehicle() {
+            this.fetchCarMakes().then(() => {
+                this.formState = "stepOne";
+            });
         },
 
-        props: ['user'],
-
-        data() {
-            return {
-                formState: 'none',
-                carMakes: null,
-                carModels: null,
-                carYears: null,
-                processing: false,
-                recentlySuccessful: false,
-                newCars: null,
-            }
+        selectMake(make) {
+            this.fetchCarModelsByMake(make).then(() => {
+                if (this.newCars === null) {
+                    this.newCars = [];
+                }
+                this.newCars.push({});
+                let item = this.newCars[this.newCars.length - 1];
+                item.make = make;
+                this.$set(this.newCars, this.newCars.length - 1, item);
+                this.formState = "stepTwo";
+            });
         },
 
-        mounted() {
-            this.newCars = JSON.parse(this.user.cars)
+        selectModel(modelData) {
+            let item = this.newCars[this.newCars.length - 1];
+            item.model = modelData.model;
+            this.$set(this.newCars, this.newCars.length - 1, item);
+
+            this.carYears = JSON.parse(modelData.years, true).map((item) =>
+                item.replace(" ", "")
+            );
+
+            this.formState = "stepThree";
         },
 
-        methods: {
-            addNewVehicle() {
-                this.fetchCarMakes().then(() => {
-                    this.formState = 'stepOne'
+        selectYear(year) {
+            let item = this.newCars[this.newCars.length - 1];
+            item.year = year;
+            this.$set(this.newCars, this.newCars.length - 1, item);
+
+            this.formState = "none";
+        },
+
+        removeCar(carIndex) {
+            this.newCars.splice(carIndex, 1);
+        },
+
+        fetchCarMakes() {
+            return axios
+                .get(route("car.makes"))
+                .then((response) => {
+                    this.carMakes = Object.values(response.data).sort((a, b) =>
+                        a.localeCompare(b)
+                    );
                 })
-            },
-
-            selectMake(make) {
-                this.fetchCarModelsByMake(make).then(() => {
-                    if (this.newCars === null) {
-                        this.newCars = []
-                    }
-
-                    this.newCars.push({})
-
-                    let item = this.newCars[this.newCars.length - 1]
-                    item.make = make
-                    this.$set(this.newCars, this.newCars.length - 1, item)
-
-                    this.formState = 'stepTwo'
-                })
-            },
-
-            selectModel(modelData) {
-                let item = this.newCars[this.newCars.length - 1]
-                item.model = modelData.model
-                this.$set(this.newCars, this.newCars.length - 1, item)
-
-                this.carYears = JSON.parse(modelData.years, true).map(item => item.replace(' ', ''))
-
-                this.formState = 'stepThree'
-            },
-
-            selectYear(year) {
-                let item = this.newCars[this.newCars.length - 1]
-                item.year = year
-                this.$set(this.newCars, this.newCars.length - 1, item)
-
-                this.formState = 'none'
-            },
-
-            removeCar(index) {
-                this.newCars.splice(index, 1);
-            },
-
-            fetchCarMakes() {
-                return axios.get(route('car.makes'))
-                    .then(response => {
-                        this.carMakes = Object.values(response.data).sort((a, b) => a.localeCompare(b))
-                    })
-                    .catch((error) => {
-                        console.error(error)
-                        console.log('Failed to fetch car makes!')
-                    })
-            },
-
-            fetchCarModelsByMake(make) {
-                return axios.get(route('car.models', { _query: { make } }))
-                    .then(response => {
-                        this.carModels = response.data.sort((a, b) => a.model.localeCompare(b.model))
-                    })
-                    .catch((error) => {
-                        console.error(error)
-                        console.log('Failed to fetch car models!')
-                    })
-            },
-
-            updateProfileCars() {
-                this.processing = true
-
-                axios.put(route('user.cars'), this.newCars)
-                    .then(() => {
-                        this.processing = false
-
-                        this.recentlySuccessful = true
-                        setTimeout(() => {
-                            this.recentlySuccessful = false
-                        }, 3000);
-                    })
-                    .catch((error) => {
-                        console.error(error)
-                        console.log('Failed to update user cars!')
-                    })
-            },
+                .catch((error) => {
+                    console.error(error);
+                    console.log("Failed to fetch car makes!");
+                });
         },
-    }
+
+        fetchCarModelsByMake(make) {
+            return axios
+                .get(route("car.models", { _query: { make } }))
+                .then((response) => {
+                    this.carModels = response.data.sort((a, b) =>
+                        a.model.localeCompare(b.model)
+                    );
+                })
+                .catch((error) => {
+                    console.error(error);
+                    console.log("Failed to fetch car models!");
+                });
+        },
+
+        updateProfileCars() {
+            this.processing = true;
+
+            axios
+                .put(route("user.cars"), this.newCars)
+                .then(() => {
+                    this.processing = false;
+
+                    this.recentlySuccessful = true;
+                    setTimeout(() => {
+                        this.recentlySuccessful = false;
+                    }, 3000);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    console.log("Failed to update user cars!");
+                });
+        },
+    },
+};
 </script>
